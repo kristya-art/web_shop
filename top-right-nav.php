0ee1b71461660780c78795cbfdc18efc7e5d07ce
Shiryagina Kristina <kristina.shiryagina@students.bfh.ch>
2018-10-29 22:59:50 +0100

    from html code  to php dynamic menu

--- top-right-nav.php
+++ top-right-nav.php
@@ -1,10 +1,26 @@
-
+<div class="topnav"  id="myTopnav">
 <?php
 $menuitems = array("Home", "News", "Contact", "Dropdown");
 foreach ($menuitems as $key=>$value ){
-    echo($value);
+    echo '<a href="' .$value.'php">'.$value.'</a>';
 }
+
 ?>
+    <div class="dropdown">
+        <button class="dropbtn">Dropdown
+            <i class="fa fa-caret-down"></i></button>
+      <div class="dropdown-content">
+
+    <?php
+    $dropdownitems = array("Link1", "Link2", "Link3", "Link4");
+    foreach ($dropdownitems as $key=>$value ){
+    echo '<a href="'.$value.'php">' .$value.'</a>';
+}
+
+
+?>
+</div>
+</div>
 
 <!--<div class="topnav" id="myTopnav">-->
 <!--    <a href="home" class="active">Home</a>-->
@@ -13,7 +29,7 @@ foreach ($menuitems as $key=>$value ){
 <!--    <a href="#contact">Contact</a>-->
 <!--    <div class="dropdown">-->
 <!--        <button class="dropbtn">Dropdown-->
-<!--<!--            <i class="fa fa-caret-down"></i>-->-->
+<!--           <i class="fa fa-caret-down"></i>-->
 <!--        </button>-->
 <!--        <div class="dropdown-content">-->
 <!--            <a href="#">Link 1</a>-->
@@ -22,7 +38,7 @@ foreach ($menuitems as $key=>$value ){
 <!--        </div>-->
 <!--    </div>-->
 <!--    <a href="#about">About</a>-->
-<!--    <a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>-->
+
 <!--</div>-->
 
 <!--<div style="padding-left:16px">-->
@@ -31,6 +47,8 @@ foreach ($menuitems as $key=>$value ){
 <!--    <p>Hover over the dropdown button to open the dropdown menu.</p>-->
 <!--</div>-->
 
+<a href="javascript:void(0);" style="font-size:15px;" class="icon" onclick="myFunction()">&#9776;</a>
+</div>
 <script>
     function myFunction() {
         var x = document.getElementById("myTopnav");
@@ -39,9 +57,9 @@ foreach ($menuitems as $key=>$value ){
             x.className += " responsive";
             y.className += "responsive";
         } else {
-            ( x.className = "topnav")&& (y.className = "icon-bar");
+            (( x.className = "topnav")&& (y.className = "icon-bar")); //when click again the both menu's come inside of icon
         }
     }
 </script>
-
-     </html>
+  
+</html>
